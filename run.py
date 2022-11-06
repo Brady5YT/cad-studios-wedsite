1b30a3bb2a4f66ce74db7044ac6a357cf71226d0
Nathan Strong <87103168+NathanNull@users.noreply.github.com>
2022-11-06 11:23:46 -0800

    eeeeeeee

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder="site", static_folder="site/assets")
+app = Flask(__name__, template_folder="dist", static_url_path="/", static_folder="dist")
 
 @app.route("/")
 def home():
