f40155e100a436d213e41ec2e719c3dd5978be9e
Nathan Strong <87103168+NathanNull@users.noreply.github.com>
2022-11-06 13:34:19 -0800

    starting or smth

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,47 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+const navbar_height = ref("4rem")
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
+  <div class="navbar row g-0 justify-content-around">
+    <a class="col-1" href="https://vitejs.dev">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <div class="fs-1 col-10 text-capitalize text-center">Navbar stuff goes here</div>
+    <div class="col-1"></div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div class="content container-fluid d-flex flex-row justify-content-center text-center align-items-center">
+    <a href="https://vitejs.dev" target="_blank">
+      
+    </a>
+    <!-- <HelloWorld msg=" Vite+Vue . " /> -->
+  </div>
 </template>
 
 <style scoped>
 .logo {
-  height: 6em;
+  width: 3rem !important;
   padding: 1.5em;
   will-change: filter;
 }
 .logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+  filter: drop-shadow(0 0 1rem #646cffee);
 }
 .logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+  filter: drop-shadow(0 0 1rem #42b883ee);
+}
+.content {
+  background-color:rgb(20, 19, 24);
+  color: aliceblue;
+  height: calc(100vh - v-bind(navbar_height));
+}
+.navbar {
+  background-color:rgb(32, 36, 46);
+  color: aliceblue;
+  height: v-bind(navbar_height);
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,21 +1,38 @@
 <script setup>
 import { ref } from 'vue'
 
-defineProps({
+const props = defineProps({
   msg: String
 })
 
-const count = ref(0)
+const count = ref(1)
+const msg = props.msg.split("")
+
+function addCount() {
+  count.value += 1
+  console.log(msg)
+  //copy
+  let a = msg.map(a=>a)
+
+  //reverse/duplicate
+  msg.forEach((e,i,arr) => {
+    a.push(arr[arr.length-1-i])
+  });
+
+  //replace
+  msg.splice(0,msg.length)
+  a.forEach(e=>msg.push(e))
+}
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <h1>{{ msg.join("") }}</h1>
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
+  <div class="py-4 fw-light">
+    <button type="button" @click="addCount" class="btn btn-dark rounded text-light">count is {{ 2**count }}</button>
+    <p class="fw-lighter fs-5">
       Edit
-      <code>components/HelloWorld.vue</code> to test HMR
+      <code class="text-info fs-5">components/HelloWorld.vue</code> to test HMR
     </p>
   </div>
 
